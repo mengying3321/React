943d446239626cd0d6a62328b30b76317dc04a71
2273653338@qq.com <2273653338@qq.com>
2020-10-03 18:06:36 +0800

    modify

---
+++ build/precache-manifest.257a0c1ebd75df0f29297e65fc851caf.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1bfc80488883868612ed582a4ee5fe0d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fd2574f1652b285f5c2b",
+    "url": "/static/js/2.f8e35e53.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.f8e35e53.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f5dd65202cf81bd1502f",
+    "url": "/static/js/main.9b9fc99f.chunk.js"
+  },
+  {
+    "revision": "e2702865a21868f2ebe6",
+    "url": "/static/js/runtime-main.7918a0be.js"
+  }
+]);
